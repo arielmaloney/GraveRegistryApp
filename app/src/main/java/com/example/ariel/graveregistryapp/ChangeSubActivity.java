03225b05cd87be258773722857044d1f18377896
Ariel <malone23@email.franklin.edu>
2018-02-10 00:32:17 -0500

    created basic project skeleton

---
+++ app/src/main/java/com/example/ariel/graveregistryapp/ChangeSubActivity.java
@@ -0,0 +1,13 @@
+package com.example.ariel.graveregistryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangeSubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_sub);
+    }
+}
