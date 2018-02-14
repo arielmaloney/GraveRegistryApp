619f5d34f0bddb2b63c38a91c266b3e1d69d78e3
Ariel <malone23@email.franklin.edu>
2018-02-14 17:19:52 -0500

    created Login and Register Activity GUI and basic navigation functionality. Added logo image.

--- app/src/main/java/com/example/ariel/graveregistryapp/LoginActivity.java
+++ app/src/main/java/com/example/ariel/graveregistryapp/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.ariel.graveregistryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                // On Sign In button click will transfer User to Dashboard
+                Intent i = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
+
+    public void goToRegister(View view)
+    {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/ariel/graveregistryapp/RegisterActivity.java
+++ app/src/main/java/com/example/ariel/graveregistryapp/RegisterActivity.java
@@ -1,7 +1,11 @@
 package com.example.ariel.graveregistryapp;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final Button signInButton = findViewById(R.id.register_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                // On Sign In button click will transfer User to Dashboard
+                Intent i = new Intent(RegisterActivity.this, DashboardActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+
+    public void goToLogin(View view)
+    {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
