70c1caabb989fd2ac5e3e721802b3d1ebbf71231
Ariel <malone23@email.franklin.edu>
2018-02-16 01:14:13 -0500

    minor changes to Login and Register UI. added some error handling for Login and Register

--- app/src/main/java/com/example/ariel/graveregistryapp/LoginActivity.java
+++ app/src/main/java/com/example/ariel/graveregistryapp/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,10 +15,35 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Retrieves the values from the EditText fields
+        final EditText signin_email = (EditText) findViewById(R.id.et_email_signin);
+        final EditText signin_password = (EditText) findViewById(R.id.et_password_signin);
         final Button signInButton = findViewById(R.id.sign_in_button);
+
+
+
+        // A listener that waits for the signInButton to be pressed
         signInButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+
+                //Creates string objects of the email and password entered
+                final String signin_email_string = signin_email.getText().toString();
+                final String signin_password_string = signin_password.getText().toString();
+
+                // If fields are left empty than send error message to User
+                if (signin_email_string.matches("")) {
+                    Toast.makeText(LoginActivity.this, "Email is required.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if (signin_password_string.matches("")) {
+                    Toast.makeText(LoginActivity.this, "Password is required.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if (!isEmailValid(signin_email_string)) {
+                    Toast.makeText(LoginActivity.this, "Not a valid email address.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 // On Sign In button click will transfer User to Dashboard
                 Intent i = new Intent(LoginActivity.this, DashboardActivity.class);
                 startActivity(i);
@@ -25,10 +52,16 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    // Sends the User to the Register Activity when the TextView is clicked
     public void goToRegister(View view)
     {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    //Determines if email address is valid
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
 
--- app/src/main/java/com/example/ariel/graveregistryapp/RegisterActivity.java
+++ app/src/main/java/com/example/ariel/graveregistryapp/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,10 +16,24 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // Retrieves the values from the EditText fields
+        final EditText register_email = (EditText) findViewById(R.id.et_email_register);
+
+
+
         final Button signInButton = findViewById(R.id.register_button);
         signInButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+
+                // Creates string objects from the EditText objects
+                final String register_email_string = register_email.getText().toString();
+
+                if (!isEmailValid(register_email_string)) {
+                    Toast.makeText(RegisterActivity.this, "Not a valid email address.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 // On Sign In button click will transfer User to Dashboard
                 Intent i = new Intent(RegisterActivity.this, DashboardActivity.class);
                 startActivity(i);
@@ -30,5 +46,10 @@ public class RegisterActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    //Determines if email address is valid
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
