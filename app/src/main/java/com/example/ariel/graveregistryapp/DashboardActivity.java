4f58d203b3e275cfc5ff1bd746cfeef836bb1d5f
Ariel <malone23@email.franklin.edu>
2018-02-17 11:45:50 -0500

    added Grave Registry button for testing.

--- app/src/main/java/com/example/ariel/graveregistryapp/DashboardActivity.java
+++ app/src/main/java/com/example/ariel/graveregistryapp/DashboardActivity.java
@@ -1,7 +1,11 @@
 package com.example.ariel.graveregistryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,17 +13,19 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        final TextView welcomeMsg = findViewById(R.id.tvWelcomeMessage);
+
+        final Button graveRegButton = findViewById(R.id.graveRegButton);
+
+        graveRegButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // On Grave Registry button click will transfer User to Grave Registry Activity
+                Intent iGraveReg = new Intent(DashboardActivity.this, GraveRegistryActivity.class);
+                startActivity(iGraveReg);
+            }
+
+        });
     }
 
-    void testMethod() {
-        // this does nothing its a test -- Ariel
-    }
-
-    void testMethodHollyCallowa() {
-        // this also does nothing. -- Holly
-    }
-
-    void testMethodColtonHlad() {
-        // This Method does nothing at all -- Colton
-    }
 }
