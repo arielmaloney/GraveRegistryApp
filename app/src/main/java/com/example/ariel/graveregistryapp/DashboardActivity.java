3da930a5d8fe6239cea529a4b2396f912dafba5d
Ariel <malone23@email.franklin.edu>
2018-02-25 14:21:19 -0500

    added Dashboard button functionality

--- app/src/main/java/com/example/ariel/graveregistryapp/DashboardActivity.java
+++ app/src/main/java/com/example/ariel/graveregistryapp/DashboardActivity.java
@@ -17,9 +17,12 @@ public class DashboardActivity extends AppCompatActivity {
         final TextView welcomeMsg = findViewById(R.id.tvWelcomeMessage);
 
         final Button graveRegButton = findViewById(R.id.graveRegButton);
+        final Button verifyGraveButton = findViewById(R.id.verifyGraveButton);
+        final Button pastSubsButton = findViewById(R.id.viewPastSubButton);
         final Button logoutButton = findViewById(R.id.logoutButton);
 
 
+        //Sets a OnClickListener to wait for the Grave Registry Button to be clicked and navigate the User
         graveRegButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // On Grave Registry button click will transfer User to Grave Registry Activity
@@ -29,6 +32,27 @@ public class DashboardActivity extends AppCompatActivity {
 
         });
 
+        //Sets a OnClickListener to wait for the Verify Grave Button to be clicked and navigate the User
+        verifyGraveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // On Grave Registry button click will transfer User to Verifiable Activity
+                Intent iVerifyGrave = new Intent(DashboardActivity.this, VerifiableActivity.class);
+                startActivity(iVerifyGrave);
+            }
+
+        });
+
+        //Sets a OnClickListener to wait for the View Past Submissions Button to be clicked and navigate the User
+        pastSubsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // On Grave Registry button click will transfer User to Past Subs Activity
+                Intent iPastSubs = new Intent(DashboardActivity.this, PastSubsActivity.class);
+                startActivity(iPastSubs);
+            }
+
+        });
+
+        //Sets a OnClickListener to wait for the Logout Button to be clicked and navigate the User
         logoutButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // On Logout button click will Logout User and transfer back to Login page
