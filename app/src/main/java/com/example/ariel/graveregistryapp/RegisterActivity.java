bf49a6040e37a5e040b70bc5fc7bff1fdbdf1511
Ariel <malone23@email.franklin.edu>
2018-03-03 15:17:41 -0500

    Register Activity back-end code complete

--- app/src/main/java/com/example/ariel/graveregistryapp/RegisterActivity.java
+++ app/src/main/java/com/example/ariel/graveregistryapp/RegisterActivity.java
@@ -2,14 +2,15 @@ package com.example.ariel.graveregistryapp;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.speech.RecognizerIntent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -18,22 +19,26 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class RegisterActivity extends AppCompatActivity {
 
-    private static final String HttpUrl = "http://localhost/RegisterUser.php";
+    // URL to the insert_user php file
+    private static final String HttpUrl = "http://10.0.2.2/register_user.php";
 
+    // EditText variables to hold Edit Text values
     EditText register_email, register_firstname, register_lastname, register_password, register_confirmpass;
+
+    // Register button field
     Button registerButton;
+
+    // String variables to hold Edit Text values
     String register_firstName_string, register_lastName_string, register_email_string,
         register_password_string, register_confirmpass_string;
 
-    ProgressDialog progressDialog;
+    // Progress Dialog
+    private ProgressDialog progressDialog;
+
+    //Creating Volley RequestQueue
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +52,12 @@ public class RegisterActivity extends AppCompatActivity {
         register_password = findViewById(R.id.et_reg_password);
         register_confirmpass = findViewById(R.id.et_confrimPassword);
         registerButton = findViewById(R.id.register_button);
+
+        // Creating Volley newRequestQueue
+        requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+
+        // Creating the ProgressDialog
+        progressDialog = new ProgressDialog(RegisterActivity.this);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -74,71 +85,67 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
                 else {
-                    UserRegistration();
+                    progressDialog.setMessage("Please Wait, we are creating a new user account.");
+                    progressDialog.show();
+
+                    // Creating string request with post method.
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUrl,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String ServerResponse) {
+
+                                    // Hiding the progress dialog after all task complete.
+                                    progressDialog.dismiss();
+
+                                    // Showing response message coming from server.
+                                    Toast.makeText(RegisterActivity.this, ServerResponse, Toast.LENGTH_LONG).show();
+
+                                    if (ServerResponse.toString().matches("Successful Registration!")) {
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                    }
+
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError volleyError) {
+
+                                    // Hiding the progress dialog after all task complete.
+                                    progressDialog.dismiss();
+
+                                    // Showing error message if something goes wrong.
+                                    Toast.makeText(RegisterActivity.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+                                }
+                            }) {
+                        @Override
+                        protected Map<String, String> getParams() {
+
+                            // Creating Map String Params.
+                            Map<String, String> params = new HashMap<>();
+
+                            // Adding All values to Params.
+                            params.put("first_name", register_firstName_string);
+                            params.put("last_name", register_lastName_string);
+                            params.put("email", register_email_string);
+                            params.put("password", register_password_string);
+
+                            return params;
+                        }
+
+                    };
+
+                    // Creating RequestQueue.
+                    RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+
+                    // Adding the StringRequest object into requestQueue.
+                    requestQueue.add(stringRequest);
                 }
-
-
-
-
             }
         });
     }
 
-    public void UserRegistration(){
 
-        // Showing progress dialog at user registration time.
-        progressDialog.setMessage("Please Wait, We are Inserting Your Data on Server");
-        progressDialog.show();
-
-        // Creating string request with post method.
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUrl,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String ServerResponse) {
-
-                        // Hiding the progress dialog after all task complete.
-                        progressDialog.dismiss();
-
-                        // Showing Echo Response Message Coming From Server.
-                        Toast.makeText(RegisterActivity.this, ServerResponse, Toast.LENGTH_LONG).show();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-
-                        // Hiding the progress dialog after all task complete.
-                        progressDialog.dismiss();
-
-                        // Showing error message if something goes wrong.
-                        Toast.makeText(RegisterActivity.this, volleyError.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-
-                // Creating Map String Params.
-                Map<String, String> params = new HashMap<String, String>();
-
-                // Adding All values to Params.
-                // The firs argument should be same sa your MySQL database table columns.
-                params.put("first_name", register_firstName_string);
-                params.put("last_name", register_lastName_string);
-                params.put("email", register_email_string);
-                params.put("password", register_password_string);
-
-                return params;
-            }
-
-        };
-
-        // Creating RequestQueue.
-        RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
-
-        // Adding the StringRequest object into requestQueue.
-        requestQueue.add(stringRequest);
-
-    }
 
     public void goToLogin(View view)
     {
